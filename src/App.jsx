65dcf9fd0e737dc05f8daeae8587049567c19269
vmhieu <hieuminh2620@gmail.com>
2025-02-28 00:57:44 +0700

    feat: implement routing with react-router-dom and add admin layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Services from './components/Services';
@@ -9,22 +10,28 @@ import Footer from './components/Footer';
 import AdminLayout from './components/Admin/AdminLayout';
 
 function App() {
-  const isAdminRoute = window.location.pathname === '/admin';
-
-  if (isAdminRoute) {
-    return <AdminLayout />;
-  }
-
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      <Services />
-      <About />
-      <Gallery />
-      <Contact />
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        {/* Route cho trang chính */}
+        <Route
+          path="/"
+          element={
+            <div>
+              <Navbar />
+              <Hero />
+              <Services />
+              <About />
+              <Gallery />
+              <Contact />
+              <Footer />
+            </div>
+          }
+        />
+        {/* Route cho trang admin */}
+        <Route path="/admin" element={<AdminLayout />} />
+      </Routes>
+    </Router>
   );
 }
 
